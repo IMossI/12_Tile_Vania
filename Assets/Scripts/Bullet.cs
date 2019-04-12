32b41cd53a04caee1537671bc38e63c3dae89a4e
IMossI <41725345+IMossI@users.noreply.github.com>
2019-04-11 17:10:49 -1000

    Got Shooting to work

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -6,9 +6,17 @@ public class Bullet : MonoBehaviour {
 
     public float speed = 20f;
     public Rigidbody2D rb;
-	// Use this for initialization
-	void Start () {
-        rb.velocity = transform.right * speed;
+
+   
+    // Use this for initialization
+    void Start () {
+       rb.velocity = transform.right * speed;
+
 	}
 
+    void OnTriggerEnter2D (Collider2D hitInfo)
+    {
+        Debug.Log(hitInfo.name);    
+        Destroy(gameObject);
+    }
 }
